19190404a46a295f6f942e79d12a410b60393e0e
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-13 16:11:45 +0100

    control de excepciones

--- operaciones.py
+++ operaciones.py
@@ -1,15 +1,48 @@
 def suma(num1, num2):
-    sum = num1 + num2
+    try:
+        sum = num1 + num2
+    except TypeError:
+        print('Introduzca números, por favor: ')
+        num1 = int(input('Primer numero: '))
+        num2 = int(input('Segundo número: '))
+        sum = num1 + num2
     return sum
 
 def resta(num1, num2):
-    res = num1 - num2
+    try:
+        res = num1 - num2
+    except TypeError:
+        print('Introduzca dos numeros, por favor: ')
+        num1 = int(input('Primer numero: '))
+        num2 = int(input('Segundo número: '))
+        res = num1 - num2
     return res
 
 def producto(num1, num2):
-    prod = num1*num2
+    try:
+        prod = num1*num2
+    except TypeError:
+        print('Introduzca dos numeros, por favor: ')
+        num1 = int(input('Primer numero: '))
+        num2 = int(input('Segundo número: '))
+        prod = num1*num2
     return prod
 
 def division(num1, num2):
-    div = num1/num2
+    try:
+        div = num1/num2
+    except ZeroDivisionError:
+        print('No se puede dividir entre 0. Vuelva a introducir los numeros: ')
+        num1 = int(input('Primer numero: '))
+        num2 = int(input('Segundo número: '))
+        div = num1/num2
     return div
+
+a = suma(1, 2)
+print('Suma: ', a)
+
+b = resta(2, 'bb')
+print('Resta: ', b)
+
+d = division(3, 0)
+print(d)
