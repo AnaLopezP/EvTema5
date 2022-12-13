eb3f33033fd03cb04ce77bbb3d6037faef4f82e9
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-13 16:37:38 +0100

    asin si

--- operaciones.py
+++ operaciones.py
@@ -2,7 +2,7 @@ def suma(num1, num2):
     try:
         sum = num1 + num2
     except TypeError:
-        print('Introduzca números, por favor: ')
+        print('Error: tipo de dato no válido. Introduzca números, por favor: ')
         num1 = int(input('Primer numero: '))
         num2 = int(input('Segundo número: '))
         sum = num1 + num2
@@ -12,7 +12,7 @@ def resta(num1, num2):
     try:
         res = num1 - num2
     except TypeError:
-        print('Introduzca dos numeros, por favor: ')
+        print('Error: tipo de dato no válido. Introduzca dos numeros, por favor: ')
         num1 = int(input('Primer numero: '))
         num2 = int(input('Segundo número: '))
         res = num1 - num2
@@ -22,7 +22,7 @@ def producto(num1, num2):
     try:
         prod = num1*num2
     except TypeError:
-        print('Introduzca dos numeros, por favor: ')
+        print('Error: tipo de dato no válido. Introduzca dos numeros, por favor: ')
         num1 = int(input('Primer numero: '))
         num2 = int(input('Segundo número: '))
         prod = num1*num2
@@ -32,7 +32,7 @@ def division(num1, num2):
     try:
         div = num1/num2
     except ZeroDivisionError:
-        print('No se puede dividir entre 0. Vuelva a introducir los numeros: ')
+        print('Error: No se puede dividir entre 0. Vuelva a introducir los numeros: ')
         num1 = int(input('Primer numero: '))
         num2 = int(input('Segundo número: '))
         div = num1/num2
