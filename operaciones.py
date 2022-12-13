85678565fbd4f054da4943c237af242640641630
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-13 15:56:23 +0100

    creacion funciones basica

---
+++ operaciones.py
@@ -0,0 +1,15 @@
+def suma(num1, num2):
+    sum = num1 + num2
+    return sum
+
+def resta(num1, num2):
+    res = num1 - num2
+    return res
+
+def producto(num1, num2):
+    prod = num1*num2
+    return prod
+
+def division(num1, num2):
+    div = num1/num2
+    return div
