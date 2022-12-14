50be435800bfbf0d6c8a220f677546a2f2158b96
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-14 17:20:50 +0100

    funcioncomprobar

--- gestor.py
+++ gestor.py
@@ -1,7 +1,20 @@
 from pickle import *
 
 class Personaje():
-    pass
+    def __init__(self, vida, ataq, defen, alcance):
+        self.vida = vida
+        self.ataq = ataq
+        self.defen = defen
+        self.alcance = alcance
+        #poner las condiciones con la funcion comprobar
+
+    def comprobar(self, valor):
+        if valor > 0:
+            print('Tu personaje se ha creado correctamente')
+            return True
+        else: 
+            print('Las características del personaje tienen que ser mayores que 0. Vuelva a introducir los datos.')
+            return False
 
 class Gestor():
     pass
