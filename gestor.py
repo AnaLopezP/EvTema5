b225d21cc3f1e63ef8a8ac2d92d305d42996840d
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-14 17:25:24 +0100

    str y prueba

--- gestor.py
+++ gestor.py
@@ -16,5 +16,11 @@ class Personaje():
             print('Las características del personaje tienen que ser mayores que 0. Vuelva a introducir los datos.')
             return False
 
+    def __str__(self):
+        return "VIDA: " +  str(self.vida) + "\n" + "ATAQUE: " +  str(self.ataq) + "\n" + "DEFENSA: " +  str(self.defen) + "\n" + "ALCANCE: " +  str(self.alcance) + "\n"
+
 class Gestor():
     pass
+
+pepe = Personaje(3, 4, 5, 7)
+print(pepe)
