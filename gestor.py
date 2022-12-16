d8b8f7c896f5479aa565dd4a220bb926ab2546f2
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-16 17:38:17 +0100

    cositas

--- gestor.py
+++ gestor.py
@@ -1,7 +1,8 @@
 from pickle import *
 
 class Personaje():
-    def __init__(self, vida, ataq, defen, alcance):
+    def __init__(self, vida, ataq, defen, alcance, nombre):
+        self.nombre = nombre
         self.vida = vida
         self.ataq = ataq
         self.defen = defen
@@ -31,27 +32,38 @@ class Personaje():
             return False
 
     def __str__(self):
-        return "VIDA: " +  str(self.vida) + "\n" + "ATAQUE: " +  str(self.ataq) + "\n" + "DEFENSA: " +  str(self.defen) + "\n" + "ALCANCE: " +  str(self.alcance) + "\n"
+        return "NOMBRE: " +  str(self.nombre) + "\n" + "VIDA: " +  str(self.vida) + "\n" + "ATAQUE: " +  str(self.ataq) + "\n" + "DEFENSA: " +  str(self.defen) + "\n" + "ALCANCE: " +  str(self.alcance) + "\n"
 
 class Gestor():
     def __init__(self, personaje):
         self.personaje = personaje
 
     def añadir(self, fichero):
-        dump(self.personaje, fichero)
-        fichero.close()
+        f = open(fichero, 'wb')
+        dump(self.personaje, f)
+        f.close()
 
     def eliminar(self):
         pass
 
-    def mostrar(self):
-        pass
+    def mostrar(self, fichero):
+        f = open(fichero, 'rb')
+        datos = load(f)
+        print(datos)
+        f.close()
+        
 
     
-
-p = Personaje(3, 4, 61, 7)
-fichero = open('personajes.pckl', 'wb')
+lista = []
+p = Personaje(3, 4, 61, 7, 'pepe')
+a = Personaje(4, 4, 4, 4, 'anabelle')
+#ichero = open('personajes.pckl', 'wb')
 pepe = Gestor(p)
-print(pepe.personaje)
-pepe.añadir(fichero)
-load(fichero)
+anabelle = Gestor(a)
+lista.append(pepe)
+lista.append(anabelle)
+#print(pepe.personaje)
+print(lista)
+for i in range(len(lista)):
+    lista[i].añadir('personajes.pckl')
+    lista[i].mostrar('personajes.pckl')
