7e34b2733a6a632340cfa3056647e22f72b82bc9
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-12-16 17:56:23 +0100

    vuelvo a empezar

--- gestor.py
+++ gestor.py
@@ -35,35 +35,6 @@ class Personaje():
         return "NOMBRE: " +  str(self.nombre) + "\n" + "VIDA: " +  str(self.vida) + "\n" + "ATAQUE: " +  str(self.ataq) + "\n" + "DEFENSA: " +  str(self.defen) + "\n" + "ALCANCE: " +  str(self.alcance) + "\n"
 
 class Gestor():
-    def __init__(self, personaje):
-        self.personaje = personaje
-
-    def añadir(self, fichero):
-        f = open(fichero, 'wb')
-        dump(self.personaje, f)
-        f.close()
-
-    def eliminar(self):
-        pass
-
-    def mostrar(self, fichero):
-        f = open(fichero, 'rb')
-        datos = load(f)
-        print(datos)
-        f.close()
+ 
         
 
-    
-lista = []
-p = Personaje(3, 4, 61, 7, 'pepe')
-a = Personaje(4, 4, 4, 4, 'anabelle')
-#ichero = open('personajes.pckl', 'wb')
-pepe = Gestor(p)
-anabelle = Gestor(a)
-lista.append(pepe)
-lista.append(anabelle)
-#print(pepe.personaje)
-print(lista)
-for i in range(len(lista)):
-    lista[i].añadir('personajes.pckl')
-    lista[i].mostrar('personajes.pckl')
